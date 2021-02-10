AlexeiDrozdovSecurrency/.NetTest

Added an initial baseline

Identity.API/Entities/UserEntity.cs
@@ -1,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestWeb.API.Entities
+{
+    public class UserEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+   
+        [Required]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+        
+        [Required]
+        public string Email { get; set; }
+        
+        public AddressEntity AddressEntity { get; set; }
+    }
+}

Identity.API/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -1,0 +1,103 @@
+﻿// <auto-generated />
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace TestWeb.API.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+#pragma warning disable 612, 618
+            modelBuilder
+                .UseIdentityColumns()
+                .HasAnnotation("Relational:MaxIdentifierLength", 128)
+                .HasAnnotation("ProductVersion", "5.0.2");
+
+            modelBuilder.Entity("Test.API.Persistence.AddressEntity", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("bigint")
+                        .UseIdentityColumn();
+
+                    b.Property<string>("City")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Country")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<DateTime>("CreatedAt")
+                        .HasColumnType("datetime2");
+
+                    b.Property<string>("FlatNumber")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("PostalCode")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("State")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Street")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("StreetNumber")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<DateTime>("UpdatedAt")
+                        .HasColumnType("datetime2");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Addresses");
+                });
+
+            modelBuilder.Entity("Test.API.Persistence.UserEntity", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("bigint")
+                        .UseIdentityColumn();
+
+                    b.Property<long?>("AddressEntityId")
+                        .HasColumnType("bigint");
+
+                    b.Property<string>("Email")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("FirstName")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("LastName")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("AddressEntityId");
+
+                    b.ToTable("Users");
+                });
+
+            modelBuilder.Entity("Test.API.Persistence.UserEntity", b =>
+                {
+                    b.HasOne("Test.API.Persistence.AddressEntity", "AddressEntity")
+                        .WithMany()
+                        .HasForeignKey("AddressEntityId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.Navigation("AddressEntity");
+                });
+#pragma warning restore 612, 618
+        }
+    }
+}

Identity.API/Models/UserRequest.cs
@@ -1,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TestWeb.API.Models
+{
+    public class UserRequest
+    {
+        [Required]
+        public string FirstName { get; set; }
+     
+        [Required]
+        public string LastName { get; set; }
+        
+        [Required]
+        public string Email { get; set; }
+        
+        [Required]
+        public string Country { get; set; }
+        
+        [Required]
+        public string City { get; set; }
+        
+        public string State { get; set; }
+
+        public string Street { get; set; }
+
+        public string StreetNumber { get; set; }
+        
+        public string PostalCode { get; set; }
+
+        public string FlatNumber { get; set; }
+    }
+}

stumpler/TestTask

Identity.API/Entities/UserEntity.cs
@@ -1,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestWeb.API.Entities
+{
+    public class UserEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+   
+        [Required]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+        
+        [Required]
+        public string Password { get; set; }
+        
+        [Required]
+        public string Login { get; set; }
+        
+        public AddressEntity AddressEntity { get; set; }
+    }
+}

Identity.API/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -1,0 +1,108 @@
+﻿// <auto-generated />
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using TestWeb.API;
+
+namespace TestWeb.API.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+#pragma warning disable 612, 618
+            modelBuilder
+                .UseIdentityColumns()
+                .HasAnnotation("Relational:MaxIdentifierLength", 128)
+                .HasAnnotation("ProductVersion", "5.0.2");
+
+            modelBuilder.Entity("TestWeb.API.Entities.AddressEntity", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("bigint")
+                        .UseIdentityColumn();
+
+                    b.Property<string>("City")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Country")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<DateTime>("CreatedAt")
+                        .HasColumnType("datetime2");
+
+                    b.Property<string>("FlatNumber")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("PostalCode")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("State")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Street")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("StreetNumber")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<DateTime>("UpdatedAt")
+                        .HasColumnType("datetime2");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Addresses");
+                });
+
+            modelBuilder.Entity("TestWeb.API.Entities.UserEntity", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("bigint")
+                        .UseIdentityColumn();
+
+                    b.Property<long?>("AddressEntityId")
+                        .HasColumnType("bigint");
+
+                    b.Property<string>("Login")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("FirstName")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("LastName")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Password")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("AddressEntityId");
+
+                    b.ToTable("Users");
+                });
+
+            modelBuilder.Entity("TestWeb.API.Entities.UserEntity", b =>
+                {
+                    b.HasOne("TestWeb.API.Entities.AddressEntity", "AddressEntity")
+                        .WithMany()
+                        .HasForeignKey("AddressEntityId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.Navigation("AddressEntity");
+                });
+#pragma warning restore 612, 618
+        }
+    }
+}

Identity.API/Models/UserRequest.cs
@@ -1,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TestWeb.API.Models
+{
+    public class UserRequest
+    {
+        [Required]
+        public string FirstName { get; set; }
+     
+        [Required]
+        public string LastName { get; set; }
+        
+        [Required]
+        public string Password { get; set; }
+        
+        [Required]
+        public string Login { get; set; }
+        
+        [Required]
+        public string Country { get; set; }
+        
+        [Required]
+        public string City { get; set; }
+        
+        public string State { get; set; }
+
+        public string Street { get; set; }
+
+        public string StreetNumber { get; set; }
+        
+        public string PostalCode { get; set; }
+
+        public string FlatNumber { get; set; }
+    }
+}